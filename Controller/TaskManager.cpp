a65da4ef3ea098640503a2630752a1d594be2d18
mehrozdurrani <mehrozdurrani@gmail.com>
2023-06-15 00:24:24 +0200

    Added new functions to TaskManager Classes and Interface

---
+++ Controller/TaskManager.cpp
@@ -0,0 +1,42 @@
+#include "TaskManager.h"
+
+Task *TaskManager::CreateNewTask(int taskId, std::string title, std::string dueDate, std::string priority, std::string status)
+{
+    Task *task = new Task();
+
+    task->SetTaskId(taskId);
+    task->SetTitle(title);
+    task->SetDueDate(dueDate);
+    task->SetPriority(priority);
+    task->SetStatus(status);
+
+    return task;
+}
+void TaskManager::DeleteTask(User *user, int taskId)
+{
+    // Find task from user task list with taskId and delete it.
+}
+void TaskManager::UpdateTask(User *user, int taskId, std::string title, std::string dueDate, std::string priority, std::string status)
+{
+    // Find task from user task list with taskId and update it.
+}
+void TaskManager::AssignTaskToUser(Task *task, User *user)
+{
+    user->GetUserTasks().push_back(task);
+}
+std::vector<Task *> TaskManager::GetAllUserTasks(User *user) const
+{
+    user->GetUserTasks();
+}
+std::vector<Task *> TaskManager::GetAllUserTasksSorted(User *user, std::string sortType) const
+{
+    // Get all tasks of user, sort it asendine or desending on date and return
+}
+
+TaskManager::TaskManager(/* args */)
+{
+}
+
+TaskManager::~TaskManager()
+{
+}
---
+++ Controller/TaskManager.h
@@ -0,0 +1,17 @@
+#include "ITaskManager.h"
+
+class TaskManager : public ITaskManager
+{
+private:
+    /* data */
+public:
+    TaskManager(/* args */);
+    ~TaskManager();
+
+    Task *CreateNewTask(int taskId, std::string title, std::string dueDate, std::string priority, std::string status) override;
+    void DeleteTask(User *user, int taskId) override;
+    void UpdateTask(User *user, int taskId, std::string title, std::string dueDate, std::string priority, std::string status) override;
+    void AssignTaskToUser(Task *task, User *user) override;
+    std::vector<Task *> GetAllUserTasks(User *user) const override;
+    std::vector<Task *> GetAllUserTasksSorted(User *user, std::string sortType) const override;
+};
